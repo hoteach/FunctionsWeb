4e98094ffe2bfeebfd4ee2af5999457cbc3e47bf
David Petkov <davidpetkov2006@gmail.com>
2024-10-09 10:36:10 +0300

    Add stripe key

--- HoteachApi/PaymentSession.cs
+++ HoteachApi/PaymentSession.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Stripe;
 using Stripe.Checkout;
 
 namespace HoteachApi
@@ -12,19 +13,23 @@ namespace HoteachApi
         public static IActionResult Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req)
         {
+            var stripeSecretKey = Environment.GetEnvironmentVariable("StripeSecretKey");
+            StripeConfiguration.ApiKey = stripeSecretKey;
+
             var options = new SessionCreateOptions
             {
-                PaymentMethodTypes = ["card"],
-                LineItems =
-                [
+                PaymentMethodTypes = new List<string> { "card" },
+                LineItems = new List<SessionLineItemOptions>
+                {
                     new SessionLineItemOptions
                     {
                         Price = "price_1Q7hduDfNdyg9lxWbJOsRkh3",
                         Quantity = 1,
                     },
-                ],
+                },
                 Mode = "payment",
-                SuccessUrl = "https://your-site.com/success?session_id={CHECKOUT_SESSION_ID}"
+                SuccessUrl = "https://your-site.com/success?session_id={CHECKOUT_SESSION_ID}",
+                CancelUrl = "https://your-site.com/cancel" // Optional: Add a Cancel URL
             };
 
             var service = new SessionService();
